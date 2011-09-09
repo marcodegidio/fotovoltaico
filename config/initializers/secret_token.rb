733b45a3661f7ddeebf6365e34e5142098f48c44
marcodegidio <marcodegidio@gmail.com>
2011-09-09 11:40:03 +0200

    Import iniziale

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fotovoltaico::Application.config.secret_token = '4754b5c245dbfa915bdeaa05a4ec5a13830de18611933b9040d854c29318fde66f68a4dab718661094ffc14bc0cf9d3157916136675f89cbd855131b83fb8832'
